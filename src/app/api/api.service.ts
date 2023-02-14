f09513da10a1cd4703300c2de5d6c0640f7fa750
adriencr81 <107180086+adriencr81@users.noreply.github.com>
2023-02-14 19:19:08 +0100

    back relié au front

--- src/app/api/api.service.ts
+++ src/app/api/api.service.ts
@@ -1,21 +1,22 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http'; 
 import { Observable } from 'rxjs';
-import { User } from './models/user.model';
+import { User } from '../models/user.model'; 
+import { HttpParams } from '@angular/common/http';
 
 
 
 @Injectable({
   providedIn: 'root'
 })
-export class UsersService {
+export class ApiService {
 
-  private apiBaseURL = "/compte";
+  
   constructor(private httpClient : HttpClient) {}
 
   getUsers(): Observable<User[]> {
 
-    return this.httpClient.get<User[]>( `${this.apiBaseURL} + /listerLesUser`);
+    return this.httpClient.get<User[]>( `compte/listerLesUser`);
 
   }
 
--- src/app/modules/general/liste-users/liste-users.component.ts
+++ src/app/modules/general/liste-users/liste-users.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from 'src/app/api/api.service'; 
+import { User } from 'src/app/models/user.model';
+
 
 @Component({
   selector: 'app-liste-users',
   templateUrl: './liste-users.component.html',
   styleUrls: ['./liste-users.component.css']
 })
-export class ListeUsersComponent {
+export class ListeUsersComponent implements OnInit {
+  constructor(private api:ApiService) {}
+
+  _users : User[] = [];
+
+  async ngOnInit(): Promise<void> {
+    try {
+      this._users = await this.getUsers();
+      console.log('Récupération des utilisateurs terminée.');
+    } catch (error) {
+      console.error('Une erreur est survenue lors de la récupération des utilisateurs : ', error);
+    }
+  }
+
+
+  
+
+  //get listeUser
+  getUsers(): Promise<User[]> {
+    return new Promise<User[]>((resolve, reject) => {
+      this.api.getUsers().subscribe({
+        next: (data) => {
+          resolve(data);
+        },
+        error: (err) => {
+          reject(err);
+        }
+      });
+    });
+  }
 
 }
